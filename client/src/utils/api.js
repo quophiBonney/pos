22d8b8bd2b31f8a363bcbabc36e70b71b07e8fa8
quophiBonney <quophiclef@gmail.com>
2025-10-25 13:33:50 +0000

    working live url replaced

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
-  // baseURL: "https://posbackend-ecru.vercel.app/api/",
+  // baseURL: "http://localhost:8000/api/",
+  baseURL: "https://posbackend-sigma.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
